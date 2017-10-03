c3bf9fcab08502e7a013283a10b6c0a1897bdb2b
Andrew Shopland <hi@ashp.land>
2017-10-03 14:42:01 -0700

    Implement Menu

---
+++ Contact List/InputCollector.m
@@ -0,0 +1,21 @@
+//
+//  InputCollector.m
+//  Contact List
+//
+//  Created by Andrew on 2017-10-03.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import "InputCollector.h"
+
+@implementation InputCollector
+
+-(NSString *)inputForPrompt:(NSString *)promptString
+{
+    
+    
+    
+    return @"String";
+}
+
+@end
--- Contact List/main.m
+++ Contact List/main.m
@@ -7,11 +7,45 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "InputCollector.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        InputCollector *inputCollector = [InputCollector new];
+        
+        NSArray *menuOptions = @[@"Quit", @"New", @"List", @"Show", @"Find", @"History"];
+        
+        bool doContinue = true;
+        
+        while (doContinue) {
+            NSLog(@"\nMenu Options: \n%@", menuOptions);
+            
+            //NSString *menuChoice = [inputCollector inputForPrompt:@"Choice:"];
+            NSString *fakeChoice = @"Quit";
+            
+            NSUInteger choice = [menuOptions indexOfObject:fakeChoice];
+
+            switch (choice) {
+                // quit
+                case 0:
+                    NSLog(@"Zero Case");
+                    doContinue = false;
+                    break;
+                case 1:
+                    NSLog(@"New");
+                default:
+                    NSLog(@"That's not an option");
+                    break;
+            }
+        }
+        
+        //NSString *usernameInput = [inputCollector inputForPrompt:@"Enter your username"];
+        
+        
+        
+        
+        
     }
     return 0;
 }
